ae9df743f7c0c7ec505e6bfdf93079696d5f52be
Alexander-Douglas <alexander.douglas@lycee-berthollet.com>
2025-05-23 23:44:39 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
 document.getElementById("submitBtn").addEventListener("click", () => {
-  const password = document.getElementById("ville").value;
+  const ville = document.getElementById("ville").value;
   console.log("Bouton touché")
   // Envoi la requête POST au backend Python
   fetch("/check-weather", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ password }),
+    body: JSON.stringify({ ville }),
   })
     .then((response) => response.json()) // Réponse donnée
     .then((data) => { // Retour des données JSON en Objet JS
