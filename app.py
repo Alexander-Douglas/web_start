93dc4ff09bfb27a1dabe2f2222916c309e09187b
Alexander-Douglas <alexander.douglas@lycee-berthollet.com>
2025-05-25 19:56:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,32 +15,43 @@ app = Flask(__name__)
 # API --> weather demand using an API app (OpenWeatherMap)
 # API key = dbd5bc945bb792cf9efd2af6370466fd
 # https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid=dbd5bc945bb792cf9efd2af6370466fd
-#
-lon,lat,appid = str(),str(),"dbd5bc945bb792cf9efd2af6370466fd"
-# lat,lon = '51.5072','0.1276'
-# payload = {'lat': lat, 'lon': lon, 'appid':appid}
 
-# url = "api.openweathermap.org/data/2.5/weather?"              -> lat, lon, appid, [mode, units, lang]               →  Current weather data
-# url = "api.openweathermap.org/geo/1.0/direct?"                -> q, appid, [limit]                                  →  Geocoding (Name -> Coords)
-# url = "api.openweathermap.org/data/2.5/forecast?"             -> lat, lon, appid, [units, mode, cnt, units, lang]   →  5-day forecast
-# url = "http://api.openweathermap.org/data/2.5/air_pollution?" -> lat, lon, appid                                    →  Current air pollution data
+appid = "dbd5bc945bb792cf9efd2af6370466fd"
+units = "metric"
+lang = "fr"
 
-def weather(city_name):
+# url = "api.openweathermap.org/data/2.5/weather?"       -> lat, lon, appid, [mode, units, lang]               →  Current weather data
+# url = "api.openweathermap.org/geo/1.0/direct?"         -> q, appid, [limit]                                  →  Geocoding (Name -> Coords)
+# url = "api.openweathermap.org/data/2.5/forecast?"      -> lat, lon, appid, [units, mode, cnt, units, lang]   →  5-day forecast every 3 hours
+# url = "api.openweathermap.org/data/2.5/air_pollution?" -> lat, lon, appid                                    →  Current air pollution data
+
+def get_weather(city_name):
     global payload
-    payload = {"q": city_name,"limit": 1,"appid": appid }
     global url
-    url = "http://api.openweathermap.org/geo/1.0/direct?"
-    name = requests.get(url,params=payload).json()[0]
-    print(name)
-    lat = name["lat"]
-    lon = name["lon"]
-    payload = {'lat': lat, 'lon': lon, 'appid': appid}
+    # Geocoding
+    payload = {"q": city_name,"limit": 1,"appid": appid}
+    url = "https://api.openweathermap.org/geo/1.0/direct?"
+    geocode = requests.get(url,params=payload).json()[0]
+    print(geocode)
+    lat = geocode["lat"]
+    lon = geocode["lon"]
+    # Weather data
+    payload = {'lat': lat, 'lon': lon, 'appid': appid, 'units': units, 'lang': lang}
     url = "https://api.openweathermap.org/data/2.5/weather?"
-    wea = requests.get(url,params=payload).json()
-    print(wea)
+    weather = requests.get(url,params=payload).json()
+    print(weather)
     desc = (wea['weather'][0]['description'])
-    temp = str(int((wea['main']['temp'])-273.13)) + " degrees"
-    return (city_name,desc,temp)
+    # Forecast
+    payload = {'lat': lat, 'lon': lon, 'appid': appid, 'units': units, 'lang': lang}
+    url = "https://api.openweathermap.org/data/2.5/forecast?"
+    forecast = requests.get(url,params=payload).json()
+    print(forecast)
+    # Air pollution
+    payload = {'lat': lat, 'lon': lon, 'appid': appid, 'units': units, 'lang': lang}
+    url = "https://api.openweathermap.org/data/2.5/air_pollution?"
+    air_poll = requests.get(url,params=payload).json()
+    print(air_poll)
+    return (name,weather,forecast,air_poll)
 
 
 # print(requests.get('https://api.openweathermap.org/data/2.5/forecast?lat=51.5072&lon=0.1276&appid=dbd5bc945bb792cf9efd2af6370466fd'))
@@ -56,8 +67,8 @@ def index():
 def check_weather():
     data = request.get_json()
     user_input = data.get("ville", "")
-    a = weather(user_input)
-    ret_data = a[2]
+    data = get_weather(user_input)
+    ret_data = data[1]
     print(ret_data)
     return jsonify({"result": ret_data})
 
