ea0228c4572170c9a88fb68c382b028d68c0f282
Alexander DOUGLAS <alexander.douglas@lycee-berthollet.com>
2025-06-06 20:14:26 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -158,7 +158,7 @@ search.addEventListener("search", () => {
       date.setTime(dtTime);
       clearInterval(timeInterval);
       timeInterval = setInterval((timezone) => {
-        let now = Date.now()
+        let now = new Date()
         document.getElementById("local-time").innerHTML = `${rightJustify((now.getUTCHours()-timezone)%24,2,"0")}:${rightJustify(now.getMinutes(),2,"0")}:${rightJustify(now.getSeconds(),2,"0")} ${Math.sign(timezone)<0?"-":"+"}GMT${rightJustify(Math.abs(timezone),2,"0")}:00`
       }, 1000, timezone);
       // Calcul de cycle jour/nuit
